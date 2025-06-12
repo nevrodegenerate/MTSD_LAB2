61fdd1f576e0b90d93dd76ed55f842042c8565f5
Bogdan Lepkov <122379140+nevrodegenerate@users.noreply.github.com>
2025-06-12 19:45:44 +0300

    Finished new list method bodies

--- source/char_list.cpp
+++ source/char_list.cpp
@@ -51,7 +51,18 @@ int char_list::GetLength() {
 	return length;
 }
 
-void char_list::Append(char new_char) {}
+void char_list::Append(char new_char) {
+	char* new_list = new char[++length];
+
+	for (int i = 0; i < length - 1; i++) {
+		new_list[i] = list[i];
+	}
+
+	new_list[length - 1] = new_char;
+
+	delete[] list;
+	list = new_list;
+}
 
 void char_list::AppendAll(std::string new_chars) {
 	for (int i = 0; i < new_chars.length(); i++) {
@@ -59,10 +70,40 @@ void char_list::AppendAll(std::string new_chars) {
 	}
 }
 
-void char_list::Insert(char new_char, int index) {}
+void char_list::Insert(char new_char, int index) {
+	char* new_list = new char[++length];
+
+	for (int i = 0; i < index; i++) {
+		new_list[i] = list[i];
+	}
+
+	new_list[index] = new_char;
+
+	for (int i = index + 1; i < length; i++) {
+		new_list[i] = list[i - 1];
+	}
+
+	delete[] list;
+	list = new_list;
+}
 
 char char_list::Delete(int index) {
-	return '0';
+	char* new_list = new char[--length];
+
+	for (int i = 0; i < index; i++) {
+		new_list[i] = list[i];
+	}
+
+	char removed_char = list[index];
+
+	for (int i = index; i < length; i++) {
+		new_list[i] = list[i + 1];
+	}
+
+	delete[] list;
+	list = new_list;
+
+	return removed_char;
 }
 
 void char_list::DeleteAll(char target) {
@@ -81,13 +122,34 @@ char_list char_list::Clone() {
 	return std::move(*this);
 }
 
-void char_list::Reverse() {}
+void char_list::Reverse() {
+	char* new_list = new char[length];
+
+	for (int i = 0; i < length; i++) {
+		new_list[i] = list[length - 1 - i];
+	}
+
+	delete[] list;
+	list = new_list;
+}
 
 int char_list::FindFirst(char target) {
+	for (int i = 0; i < length; i++) {
+		if (list[i] == target) {
+			return i;
+		}
+	}
+
 	return -1;
 }
 
 int char_list::FindLast(char target) {
+	for (int i = length - 1; i >= 0; i--) {
+		if (list[i] == target) {
+			return i;
+		}
+	}
+
 	return -1;
 }
 
@@ -96,7 +158,21 @@ void char_list::Clear() {
 	list = nullptr;
 }
 
-void char_list::Extend(char_list extension) {}
+void char_list::Extend(char_list extension) {
+	char* new_list = new char[length + extension.GetLength()];
+
+	for (int i = 0; i < length; i++) {
+		new_list[i] = list[i];
+	}
+
+	for (int i = 0; i < extension.GetLength(); i++) {
+		new_list[length + i] = extension.Get(i);
+	}
+
+	length += extension.GetLength();
+	delete[] list;
+	list = new_list;
+}
 
 void char_list::Print() {
 	for (int i = 0; i < length; i++) {
